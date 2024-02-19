74e44d76f8c0ead569381747ab03c6691ed9dd9d
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-20 00:43:27 +0100

    feat: autogenerated og images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
+  latex: true,
+  flexsearch: {
+    codeblocks: false,
+  },
 });
 
 module.exports = {
   ...withNextra(),
-  images: {
-    unoptimized: true,
+  webpack(config, { nextRuntime }) { 
+    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+    if (typeof nextRuntime === "undefined") {
+      const { IgnorePlugin } = require("webpack");
+      const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
+      config.plugins.push(ignoreFs);
+    }
+
+    return config;
   },
-  experimental: {
-    esmExternals: false,
-  }
 };
